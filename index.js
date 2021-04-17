699b2a0fa166a62c9e116383a79a8bd85db78702
stgary <sgary0@protonmail.com>
2021-04-17 09:58:37 -0400

    correction of PORT variable

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require('./server/app.js');
 const { PORT } = require('config.js');
 
-app.listen(port, () => console.log(`***Server running on port ${port}***`));
+app.listen(PORT, () => console.log(`***Server running on port ${PORT}***`));
